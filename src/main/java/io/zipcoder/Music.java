5235574459394d7b93cb08d81cbf40fa5e1885ad
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-19 20:27:00 -0500

    completed lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -2,13 +2,50 @@ package io.zipcoder;
 
 public class Music {
 
+
+
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection( Integer startIndex, String selection) {
+        Integer forward = 0;
+        Integer backward = 0;
+        Integer minButtonPresses = 0;
+        for (int i = startIndex; i < getPlayList().length; i++) { //will stop adding to counter when found
+            if (!selection.equals(getPlayList()[i])) {
+                forward++;
+            }
+        }
+        for (int j = startIndex ; j >= 0; j--) {
+            if (!selection.equals(getPlayList()[j])) { // will stop adding to counter when string found
+                backward++;
+            }
+        }
+
+        if (forward < backward) {
+            minButtonPresses = forward;
+
+        } else if (forward == backward) {
+
+            minButtonPresses = forward;
+
+        } else {
+            minButtonPresses = backward;
+
+        }
+
+
+        return minButtonPresses;
+
+
+  }
+
+
+
+    public String[] getPlayList() {
+        return playList;
     }
 }
